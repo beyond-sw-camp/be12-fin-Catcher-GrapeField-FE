1b8714245ea9fd174c79cecf509f3f5e9c4e9bc1
j0a0j <jiweon.j.kim@gmail.com>
2025-04-02 11:50:18 +0900

    #6 Footer 구현

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
-import Header from './components/common/Header.vue'
+import Header from './common/Header.vue'
+import Footer from './common/Footer.vue'
 </script>
 
 <template>
-  <Header></Header>
-  <router-view></router-view>
+  <div class="layout">
+    <Header />
+    <main>
+      <router-view />
+    </main>
+    <Footer />
+  </div>
 </template>
+
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 98vh;
+}
+
+main {
+  flex: 1;
+  
+}
+</style>
