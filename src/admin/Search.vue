0c0ee85ce1c890e64ae5c9f8971eafab6181e21b
bdt6246 <a01094079599@gmail.com>
2025-04-03 11:10:49 +0900

    [fix] Search버튼 추가

--- src/admin/Search.vue
+++ src/admin/Search.vue
@@ -1,12 +1,16 @@
 <script setup>
+import { ref } from 'vue'
+const keyword = ref('')
+
+const SearchItems = (text) => {
+    console.log('검색어:', text)
+}
 </script>
 
 <template>
-    <div class="mt-2 px-10">
-        <input type="text" placeholder="검색어를 입력하세요."
-            class="bg-zinc-100 w-full h-12 px-4 rounded-3xl border border-zinc-300" />
+    <div class="mt-2 px-10 flex">
+    <input type="text" v-model="keyword" placeholder="검색어를 입력하세요." class="bg-zinc-100 w-full h-12 px-4 rounded-3xl border border-zinc-300"
+/>
+    <button class="ml-4" @click="SearchItems(keyword)">⚔️</button>
     </div>
 </template>
-
-<style scoped>
-</style>
