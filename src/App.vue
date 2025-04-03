e8538888402a856237035fa63ddf4def26e8edc1
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-04-03 14:59:33 +0900

    [Design] Header 레이아웃 여백 rem 단위로 변경

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
+import {computed} from 'vue'
+import {useRoute} from 'vue-router'
 import Header from './common/Header.vue'
 import Footer from './common/Footer.vue'
 import SideBar from './common/SideBar.vue'
@@ -12,10 +12,10 @@ const showSidebar = computed(() => !['Login', 'SignUp'].includes(route.name))
 
 <template>
   <div class="layout">
-    <Header />
-    <router-view />
-    <SideBar v-if="showSidebar" />
-    <Footer />
+    <Header/>
+    <router-view/>
+    <SideBar v-if="showSidebar"/>
+    <Footer/>
   </div>
 </template>
 
@@ -24,26 +24,32 @@ const showSidebar = computed(() => !['Login', 'SignUp'].includes(route.name))
   display: flex;
   flex-direction: column;
   min-height: 98vh;
-} 
+}
 
 main {
   flex: 1;
-  margin-top: 5vw;
-  /* 헤더 높이와 동일하게 설정 */
-  padding: 1vw;
+  margin-top: 4rem;
+  /* 헤더 높이와 동일하게 설정 (기존 5vw에서 4rem으로 변경) */
+  padding: 1rem; /* 1vw에서 1rem으로 변경 */
+}
+
+@media screen and (max-width: 1024px) {
+  main {
+    margin-top: 4.5rem; /* 헤더 높이와 일치하도록 추가 */
+  }
 }
 
 @media screen and (max-width: 768px) {
   main {
-    /* 모바일에서는 헤더 높이가 커짐 */
-    margin-top: 8vw;
+    /* 모바일에서는 헤더 높이가 커짐 (8vw에서 5rem으로 변경) */
+    margin-top: 5rem;
   }
 }
 
 @media screen and (max-width: 480px) {
   main {
-    /* 작은 화면에서 추가 여백 */
-    margin-top: 10vw;
+    /* 작은 화면에서 추가 여백 (10vw에서 5.5rem으로 변경) */
+    margin-top: 5.5rem;
   }
 }
 </style>
