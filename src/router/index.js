bb658d65c928e7558d6f7d614b77b3ed4a35b23f
s00ya <jjschoho@gmail.com>
2025-04-03 23:24:32 +0900

    메인페이지 카드, 카테고리  활용

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import CustomerCenter from "../admin/customercenter/CustomerCenter.vue"
 import Admin from "../admin/Admin.vue"
 import SignupSuccess from "../user/SignupSuccess.vue";
 import EmailVerification from "../user/EmailVerification.vue";
+import EventShowMore from "../events/EventShowMore.vue";
 
 const routes = [
     {path: "/", component: Main},
@@ -21,6 +22,7 @@ const routes = [
     {path: "/admin", component: Admin},
     {path: "/signupsuccess", name: 'SignupSuccess', component: SignupSuccess},
     { path: '/emailverify', name: EmailVerification, component: EmailVerification },
+    {path: "/events", name: 'EventShowMore', component: EventShowMore},
 ]
 
 const router = createRouter({
