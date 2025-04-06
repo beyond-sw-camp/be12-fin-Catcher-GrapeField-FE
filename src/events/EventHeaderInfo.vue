e746604d827e7fcf919f0e2ff1b1c1bc9f56a97a
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-04-07 04:53:54 +0900

    [Design] 상세페이지 정보섹션 레이아웃 치우침 조절

--- src/events/EventHeaderInfo.vue
+++ src/events/EventHeaderInfo.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="w-[1320px] h-80 relative bg-violet-50 rounded-xl mx-auto">
+    <div class="flex gap-8 h-80 bg-violet-50 rounded-xl mx-auto p-10 ">
       <!-- 포스터 이미지 박스 -->
-      <div class="w-44 h-56 absolute left-[48px] top-[67px] bg-amber-300 rounded-[10px] flex items-center justify-center">
+      <div class="w-1/6 h-56 bg-amber-300 rounded-[10px] flex items-center justify-center">
         <img :src="event.posterUrl" alt="포스터" class="w-24 h-36 object-cover" />
       </div>
   
       <!-- 공연 정보 텍스트 -->
-      <div class="absolute left-[255px] top-[72px]">
-        <div class="flex items-center space-x-2">
+      <div class="w-5/6">
+        <div class="flex items-center">
           <span class="text-2xl font-bold text-neutral-800">{{ event.genre }}</span>
           <span class="text-2xl font-bold text-neutral-800">{{ event.title }}</span>
         </div>
@@ -24,30 +24,30 @@
         </div>
   
         <!-- 공연 메타 정보 -->
-        <div class="mt-6 p-4 bg-white rounded-xl w-[1024px] shadow-sm">
-          <div class="grid grid-cols-2 gap-y-2 gap-x-10 text-sm">
+        <div class="mt-6 p-4 bg-white rounded-xl shadow-sm">
+          <div class="grid grid-cols-2 gap-y-2 gap-x-2 text-sm">
             <div class="flex">
-              <span class="w-24 font-bold text-neutral-800">공연 시간</span>
+              <span class="w-24 font-bold text-neutral-800 truncate">공연 시간</span>
               <span class="text-zinc-600">{{ event.duration }}</span>
             </div>
             <div class="flex">
-              <span class="w-24 font-bold text-neutral-800">가 격</span>
+              <span class="w-24 font-bold text-neutral-800 truncate">가 격</span>
               <span class="text-zinc-600">{{ event.price }}</span>
             </div>
             <div class="flex">
-              <span class="w-24 font-bold text-neutral-800">관람 연령</span>
+              <span class="w-24 font-bold text-neutral-800 truncate">관람 연령</span>
               <span class="text-zinc-600">{{ event.ageLimit }}</span>
             </div>
             <div class="flex">
-              <span class="w-24 font-bold text-neutral-800">선 예매</span>
+              <span class="w-24 font-bold text-neutral-800 truncate">선 예매</span>
               <span class="text-zinc-600">{{ event.bookingEarly?.period }}</span>
             </div>
             <div class="flex">
-              <span class="w-24 font-bold text-neutral-800">장 소</span>
+              <span class="w-24 font-bold text-neutral-800 truncate">장 소</span>
               <span class="text-zinc-600">{{ event.venue }}</span>
             </div>
             <div class="flex">
-              <span class="w-24 font-bold text-neutral-800">일반 예매</span>
+              <span class="w-24 font-bold text-neutral-800 truncate">일반 예매</span>
               <span class="text-zinc-600">{{ event.bookingNormal?.period }}</span>
             </div>
           </div>
