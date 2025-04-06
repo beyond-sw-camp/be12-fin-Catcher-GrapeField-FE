dcbe1a2b6b9a1dac9bedd6714ee0feb8c47b458e
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-04-07 01:58:24 +0900

    [Design] 공연/전시 페이지 반응형 grid 수정

--- src/events/EventShowMoreList.vue
+++ src/events/EventShowMoreList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper max-w-screen-xl mx-auto grid grid-cols-4 gap-x-4 gap-y-8 mt-10">
+  <div class="wrapper max-w-screen-xl mx-auto grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 gap-y-8 mt-10">
     <EventShowMoreCard
         v-for="event in events"
         :key="event.id"
