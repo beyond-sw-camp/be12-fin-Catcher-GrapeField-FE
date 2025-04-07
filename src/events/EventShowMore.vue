c1136c14b19d4104c027ab6d2fe34090f09685ed
s00ya <jjschoho@gmail.com>
2025-04-07 14:47:58 +0900

    feat: 제일 상단의 추천 카드 부분에 여백 (#147)

--- src/events/EventShowMore.vue
+++ src/events/EventShowMore.vue
@@ -1,36 +1,38 @@
 <template>
-    <div>
-      <!-- 추천 콘텐츠 섹션 -->
-       <!-- ✅ 메인에서는 더보기 버튼이 나오고 공연/전시 페이지에서는 더보기 버튼이 없도록 false 처리 -->
-      <Card title="추천 콘텐츠" :cards="combinedCards" :showMoreButton="false" />
-      <!-- 카테고리 -->
-      <Category />
-  
-      <!-- 그 아래 이벤트 카드 리스트 -->
-      <EventShowMoreList />
+  <div>
+    <!-- 추천 콘텐츠 섹션 -->
+    <div class="mt-6">
+      <!-- Tailwind 기준: mt-6은 약 1.5rem (24px) -->
+      <Card title="추천" :cards="combinedCards" :showMoreButton="false" />
     </div>
-  </template>
-  
-  <script>
-  import Card from '../main/Card.vue'
-  import cardData from '../assets/data/card.json'
-  import Category from '../main/Category.vue'
-  import EventShowMoreList from './EventShowMoreList.vue'
-  
-  export default {
-    components: {
-      Card,
-      Category,
-      EventShowMoreList,
-    },
-    data() {
-      return {
-        combinedCards: [
-          ...cardData.recommendedCards,
-          ...cardData.popularCards,
-          ...cardData.newCards
-        ]
-      };
-    }
-  }
-  </script>
+    <!-- 카테고리 -->
+    <Category />
+
+    <!-- 그 아래 이벤트 카드 리스트 -->
+    <EventShowMoreList />
+  </div>
+</template>
+
+<script>
+import Card from "../main/Card.vue";
+import cardData from "../assets/data/card.json";
+import Category from "../main/Category.vue";
+import EventShowMoreList from "./EventShowMoreList.vue";
+
+export default {
+  components: {
+    Card,
+    Category,
+    EventShowMoreList,
+  },
+  data() {
+    return {
+      combinedCards: [
+        ...cardData.recommendedCards,
+        ...cardData.popularCards,
+        ...cardData.newCards,
+      ],
+    };
+  },
+};
+</script>
