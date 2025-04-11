38be6978a5dac708c2fca24f142b4367e842ff2d
bdt6246 <a01094079599@gmail.com>
2025-04-11 16:44:44 +0900

    [fix] script -> script setup, tailwind script를 script setup형태로 바꾸고 css는 tailwind 형식으로 적용

--- src/main/CardTemplate.vue
+++ src/main/CardTemplate.vue
@@ -1,52 +1,46 @@
 <template>
-    <Card class="wrapper" title="추천" :cards="cardData.recommendedCards" />
-    <Card class="wrapper" title="인기" :cards="cardData.popularCards" />
-    <Card class="wrapper" title="신규" :cards="cardData.newCards" />
+    <div class="w-full max-w-[70vw] mx-auto my-[4vh] px-4 box-border">
+        <Card title="추천" :cards="cardData.recommendedCards" />
+    </div>
+    <div class="w-full max-w-[70vw] mx-auto my-[4vh] px-4 box-border">
+        <Card title="인기" :cards="cardData.popularCards" />
+    </div>
+    <div class="w-full max-w-[70vw] mx-auto my-[4vh] px-4 box-border">
+        <Card title="신규" :cards="cardData.newCards" />
+    </div>
 </template>
 
-<script>
+<script setup>
 import Card from './Card.vue'
 import cardData from '../assets/data/card.json'
-
-export default {
-    components: {
-        Card
-    },
-    data() {
-        return {
-            cardData
-        };
-    }
-};
 </script>
 
 <style scoped>
-/* 카드 래퍼 스타일 - 75vw로 변경 */
-.wrapper {
-    width: 100%;
-    max-width: 70vw;
-    margin: 4vh auto;
-    padding: 0 1rem;
-    box-sizing: border-box;
-}
-
 @media (max-width: 1200px) {
-    .wrapper {
+    .max-w-\[70vw\] {
         max-width: 95vw;
     }
 }
 
 @media (max-width: 768px) {
-    .wrapper {
+    .max-w-\[70vw\] {
         max-width: 90vw;
-        margin: 3vh auto;
+    }
+
+    .my-\[4vh\] {
+        margin-top: 3vh;
+        margin-bottom: 3vh;
     }
 }
 
 @media (max-width: 480px) {
-    .wrapper {
+    .max-w-\[70vw\] {
         max-width: 85vw;
-        margin: 2vh auto;
+    }
+
+    .my-\[4vh\] {
+        margin-top: 2vh;
+        margin-bottom: 2vh;
     }
 }
 </style>
