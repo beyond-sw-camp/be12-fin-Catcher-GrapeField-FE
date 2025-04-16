62956b560ea4f2a760125dc7db2607677212d061
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-04-16 15:11:17 +0900

    [fix] webSocketClientjs에 쿠키전송 설정 추가

--- src/utils/webSocketClient.js
+++ src/utils/webSocketClient.js
@@ -8,15 +8,17 @@ let stompClient = null;
 const baseUrl = import.meta.env.VITE_BASE_URL || '';  // 예: '' 또는 'http://localhost:8080'
 const socketUrl = `${baseUrl}/ws`; // 상대경로도 가능하게 설정
 
-export function connect(onConnectCallback, /* token */) {
-    const socket = new SockJS(socketUrl);
+export function connect(onConnectCallback /*, token */) {
+    const socket = new SockJS(socketUrl, null, {
+        transports: ['websocket', 'xhr-streaming', 'xhr-polling'],
+        // transports: ['websocket'], // 웹소켓만 사용하고 싶다면 이 줄을 사용
+        withCredentials: true // 쿠키 전송 시 필수!!!! 쿠키를 포함시키는 설정
+    });
     stompClient = new Client({
         webSocketFactory: () => socket,
-        // connectHeaders: token ? { Authorization: `Bearer ${token}` } : {},
         reconnectDelay: 5000,
         onConnect: () => {
             console.log('STOMP 연결 성공 (쿠키 + Interceptor 사용)');
-            // console.log(`{TOKEN ${token ? '있음' : '없음'}}`);
             console.log('WebSocket 연결 성공');
             if (onConnectCallback) onConnectCallback(stompClient);
         },
