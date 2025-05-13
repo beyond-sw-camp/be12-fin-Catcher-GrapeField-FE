1a7cbcb90c4c7549d79fefe67f7abf3700e0df38
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-05-13 12:14:47 +0900

    [fix] 사이드바 디자인 액션 변수 오류 수정

--- src/common/Sidebar.vue
+++ src/common/Sidebar.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref, reactive, computed, onMounted, nextTick, watch, onBeforeUnmount } from 'vue'
-import axios from 'axios'
 import { useRoute, useRouter } from 'vue-router'
 import { useUserStore } from '../stores/useUserStore'
 import { useChatRoomListStore } from '@/stores/useChatRoomListStore'
@@ -101,7 +100,7 @@ async function showChatRoom(room) {
   state.activeRoomIdx = room.roomIdx;
 
   await chatRoomStore.connectWebSocket(room.roomIdx);
-  await chatRoomStore.fetchChatRoom(room.roomIdx, chatBody.value);
+  await chatRoomStore.fetchChatRoom(room.roomIdx, chatBody);
   state.activeChatRoomMessages = chatRoomStore.formattedMessages;
   await nextTick(() => {
     chatRoomStore.initialScroll(chatBody.value)
@@ -407,7 +406,7 @@ onBeforeUnmount(() => {
               <!-- 채팅 메세지 목록 -->
               <div ref="chatBody" class="flex-1 overflow-y-auto space-y-3 mb-3">
                 <transition-group>
-                  <div v-for="(msg, index) in chatRoomStore.formattedMessages" :key="msg.id"
+                  <div v-for="(msg, idx) in chatRoomStore.formattedMessages" :key="msg.idx"
                     :class="['flex', msg.isMe ? 'justify-end' : 'justify-start']">
                     <div v-if="!msg.isMe" class="w-8 h-8 rounded-full bg-purple-100 overflow-hidden mr-2">
                       <img :src="msg.avatar" alt="프로필" class="w-full h-full object-cover" />
